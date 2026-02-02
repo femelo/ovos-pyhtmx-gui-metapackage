88dccc772ae5f2f211b3a946dc8a399fdbbb05da
Flávio De Melo <flavio.eler@gmail.com>
2026-02-02 21:31:25 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# pyhtmx-gui-env
-Metapackage for setting up a development environment for the PyHTMX GUI client
+# OVOS PyHTMX Metapackage
+
+This project defines a metapackage that consolidates several OVOS components for setting up a development
+environment for the PyHTMX GUI client.
+
+## Installation
+
+To install this metapackage and all its dependencies, you can use pip:
+
+```bash
+pip install .
+```
+
+This will install the following packages from their respective GitHub branches:
+
+- `ovos-gui@add-pyhtmx-system-pages`
+- `ovos-audio@add-audio-duration`
+- `ovos-dinkum-listener@add-audio-duration-to-master`
+- `ovos-skill-homescreen@add-pyhtmx-gui-resources`
+- `ovos-ocp-audio-plugin@add-pyhtmx-resources`
+- `ovos-skill-date-time@add-pyhtmx-time-page`
+- `ovos-skill-weather@add-units-to-weather-params`
+- `pyhtmx-gui-client@extract-homescreen-resources`
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ovos-pyhtmx-metapackage",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "ovos-gui @ git+https://github.com/femelo/ovos-gui.git@add-pyhtmx-system-pages",
+        "ovos-audio @ git+https://github.com/femelo/ovos-audio.git@add-audio-duration",
+        "ovos-dinkum-listener @ git+https://github.com/femelo/ovos-dinkum-listener.git@add-audio-duration-to-master",
+        "ovos-skill-homescreen @ git+https://github.com/femelo/ovos-skill-homescreen.git@add-pyhtmx-gui-resources",
+        "ovos-ocp-audio-plugin @ git+https://github.com/femelo/ovos-ocp-audio-plugin.git@add-pyhtmx-resources",
+        "ovos-skill-date-time @ git+https://github.com/femelo/ovos-skill-date-time.git@add-pyhtmx-time-page",
+        "ovos-skill-weather @ git+https://github.com/femelo/ovos-skill-weather.git@add-units-to-weather-params",
+        "pyhtmx-gui-client @ git+https://github.com/femelo/pyhtmx-gui-client.git@extract-homescreen-resources",
+    ],
+    author="Flavio Melo",
+    author_email="flavio.melo@gmail.com",
+    description="Metapackage for OVOS PyHTMX GUI components",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.8",
+)
